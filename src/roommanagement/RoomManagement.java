e79831d3876378c47a1e138bbf36db0a757ca66b
Daniel <du.ubanan@gmail.com>
2018-07-06 14:08:11 +0800

    Git asked me to commit

--- src/roommanagement/RoomManagement.java
+++ src/roommanagement/RoomManagement.java
@@ -9,34 +9,32 @@ import java.sql.*;
  */
 public class RoomManagement {
 
-    static final String DB_URL =
-      "jdbc:mysql://localhost/room_management?useUnicode=true&useJDBCCompliantTimeZoneShift=true&useLegacyDaytimeCode=false&serverTimezone=UTC";
-   static final String DB_DRV =
-      "com.mysql.jdbc.Driver";
-   static final String DB_USER = "root";
-   static final String DB_PASSWD = "";
+    static final String DB_URL = "jdbc:mysql://localhost/room_management?useUnicode=true&useJDBCCompliantTimeZoneShift=true&useLegacyDaytimeCode=false&serverTimezone=UTC";
+    static final String DB_DRV = "com.mysql.jdbc.Driver";
+    static final String DB_USER = "root";
+    static final String DB_PASSWD = "";
 
-   public static void main(String[] args){
+    public static void main(String[] args){
 
-      Connection connection = null;
-      Statement statement = null;
-      ResultSet resultSet = null;
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
-      try{
-         connection=DriverManager.getConnection
-            (DB_URL,DB_USER,DB_PASSWD);
-         statement=connection.createStatement();
-         resultSet=statement.executeQuery
-            ("SELECT * FROM user");
-         while(resultSet.next()){
-            System.out.printf("%s\n",
-            resultSet.getString(2));
-         }
-        
-      }catch(SQLException ex){
-          System.out.println(ex);
-      }
-      new Login().open();
-   }
+        try{
+           connection=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWD);//establishing connection??
+
+           statement=connection.createStatement();//establishing connection??
+
+           resultSet=statement.executeQuery("SELECT * FROM user");//query code
+
+            while(resultSet.next()){
+               System.out.printf("%s\n", resultSet.getString(2)); //printing the name
+            }
+
+        }catch(SQLException ex){
+            System.out.println(ex);
+        }
+        new Login().open();
+    }
     
 }
