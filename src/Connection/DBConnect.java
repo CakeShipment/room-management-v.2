261f3a4d666f2bab42cb2e8e587bca1e798edabb
Daniel <du.ubanan@gmail.com>
2018-07-06 14:45:06 +0800

    Added DBpull.pull(String query) method to make pulling from DB easier

--- src/Connection/DBConnect.java
+++ src/Connection/DBConnect.java
@@ -1,20 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Connection;
-
-
 import java.sql.*;
 
-/**
- *
- * @author Cake
- */
 public class DBConnect {
-    static final String DB_URL =
-      "jdbc:mysql://localhost/room_management?useUnicode=true&useJDBCCompliantTimeZoneShift=true&useLegacyDaytimeCode=false&serverTimezone=UTC";
+    static final String DB_URL = "jdbc:mysql://localhost/room_management?useUnicode=true&useJDBCCompliantTimeZoneShift=true&useLegacyDaytimeCode=false&serverTimezone=UTC";
     static final String DB_USER = "root";
     static final String DB_PASSWD = "";
     
@@ -22,8 +10,8 @@ public class DBConnect {
         Connection connection = null;
         Statement statement = null;
         try{
-            connection=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWD);
-            statement=connection.createStatement();
+            connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWD);
+            statement = connection.createStatement();
         }catch(SQLException ex){
             System.out.println(ex);
         }
